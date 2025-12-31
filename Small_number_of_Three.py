294edcadbf51e19e74ce8c38f735fa3aa851f3ad
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-12-31 23:17:30 +0600

    Add files via upload

---
+++ Small_number_of_Three.py
@@ -0,0 +1,23 @@
+# তিনটি সংখ্যার মধ্যে ছোট সংখ্যা নির্ণয়ের প্রোগ্রাম |
+
+# Pogram-
+
+a = int(input("Enter a value A =  "));
+b = int(input("Enter a value B =  "));
+c = int(input("Enter a value C =  "));
+if(a<b and a<c):
+    print("A =",a,"is the smallest number");
+elif(b<a and b<c):
+    print("B =",b,"is the smallest number");
+else:
+    print("C =",c,"is the smallest number");
+
+
+# input-
+# 47
+# 99
+# 72
+
+
+# output-
+# A = 47 is the smallest number
--- loop.py
+++ loop.py
@@ -1,15 +1,41 @@
-for i in "python":
-    print(i)
+# for i in "python":
+#     print(i)
 
-list=[10,20,30]
-for i in list:
-    print(i)
+# list=[10,20,30]
+# for i in list:
+#     print(i)
 
-for i in range(0,10,2):
-    print(i)
+# for i in range(0,10,2):
+#     print(i)
 
 # while
-a=1
-while(a<=10):
-    print(a)
-    a+=1
+# a=1
+# while(a<=10):
+#     print(a)
+#     a+=1
+
+# n=int(input("enter valu"))+1
+# i=1
+# sum=0
+# for i in range(1,n,2):
+#     sum+=i
+# print(sum)
+# while(i<=n):
+#     sum+=i
+#     i+=2
+
+# print("r",sum)
+
+# n=int(input("enter valu")) #+1
+# sum=0
+# i=3
+# while(i<=n):
+#     sum+=i*i
+#     i=i+3
+# for i in range(3,n,3):
+#     sum+=i*i
+# print(sum)
+
+n=int(input("enter valu"))
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
