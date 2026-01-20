bfffd9efb864d73773c7200a5b6eff7d32c53053
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2026-01-20 15:21:59 +0600

    Add files via upload

--- if_elif_else.py
+++ if_elif_else.py
@@ -8,31 +8,31 @@
 # else:
 #     print("kisu khabo na")
 
-# import math 
-# a=int(input("enter a valu a= "))
-# b=int(input("enter a valu b= "))
-# c=int(input("enter a valu c= "))
-# d=(b**2)-(4*a*c)
-# if(d==0):
-#     x=(-b/(2*a))
-#     print("Roots is a real number",x)
-# elif(d>0):
-#     x1= ((-b+math.sqrt(d))/(2*a))
-#     x2= ((-b-math.sqrt(d))/(2*a))
-#     print("Roots is a Real number",x1,"and",x2)
-# else:
-#     print("Roots are imaginary")
-
-import math
+import math 
 a=int(input("enter a valu a= "))
 b=int(input("enter a valu b= "))
 c=int(input("enter a valu c= "))
-if((a+b)>c and (b+c)>a and (c+a)>b):
-    s=(a+b+c)/2
-    area=math.sqrt(s*(s-a)*(s-b)*(s-c))
-    print("Triangle area",area)
+d=(b**2)-(4*a*c)
+if(d==0):
+    x=(-b/(2*a))
+    print("Roots is a real number",x)
+elif(d>0):
+    x1= ((-b+math.sqrt(d))/(2*a))
+    x2= ((-b-math.sqrt(d))/(2*a))
+    print("Roots is a Real number",x1,"and",x2)
 else:
-    print("it is not a Triangle")
+    print("Roots are imaginary")
+
+# import math
+# a=int(input("enter a valu a= "))
+# b=int(input("enter a valu b= "))
+# c=int(input("enter a valu c= "))
+# if((a+b)>c and (b+c)>a and (c+a)>b):
+#     s=(a+b+c)/2
+#     area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+#     print("Triangle area",area)
+# else:
+#     print("it is not a Triangle")
 
 # if(a>b and a>c):
 #     print(a,"is largest number")
--- loop.py
+++ loop.py
@@ -37,5 +37,12 @@
 # print(sum)
 
 n=int(input("enter valu"))
-for i in range(1,11):
+for i in range(1,10+1):
     print(n,"*",i,"=",n*i)
+
+
+# n=int(input("enter valu"))
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print(sum)
